0e1a9f88d44333f745c2aa44f94c2411112bac87
JefeProyectosMF <jefeproyectosmovilform@gmail.com>
2026-07-21 18:01:00 -0400

    Fix blank screen: set vite base to '' for relative asset paths (file:// compatibility)

--- electron/main.ts
+++ electron/main.ts
@@ -39,6 +39,7 @@ function createWindow() {
     mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+    mainWindow.webContents.openDevTools();
   }
 
   mainWindow.on("closed", () => {
